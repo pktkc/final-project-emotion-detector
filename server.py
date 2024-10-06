9700a53ec80d367fe5e30d56332877a8fd7c21bd
pktkc <kcprabir@gmail.com>
2024-10-06 03:50:24 -0400

    Pylint applied

--- server.py
+++ server.py
@@ -6,18 +6,18 @@ app = Flask("Emotion Analyzer")
 
 @app.route("/")
 def home():
+    """ This code displays home page index.html."""
     return render_template('index.html')
 
 @app.route("/emotionDetector")
-def emotionDetector():
+def emotion_detect():
+    """ This function takes user input text and gets emotion score and diplays the result."""
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
-    if result['dominant_emotion'] == None:
+    if result['dominant_emotion'] is None:
         return "<b>Invalid text! Please try again!</b>"
 
-    else:
-        return f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is <b>{result['dominant_emotion']}</b>."
-
+    return f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is <b>{result['dominant_emotion']}</b>."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
